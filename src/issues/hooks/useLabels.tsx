3ad662d575ddc1f24d519a9544cc082728357290
Bikcode <bikcodeh@gmail.com>
2023-12-25 11:54:59 -0500

    add placeholder

--- src/issues/hooks/useLabels.tsx
+++ src/issues/hooks/useLabels.tsx
@@ -1,6 +1,6 @@
 import { useQuery } from '@tanstack/react-query';
-import { githubApi } from "../../api/githubApi"
-import { Label } from "../interfaces/label"
+import { githubApi } from '../../api/githubApi'
+import { Label } from '../interfaces/label'
 import { sleep } from '../../helpers/sleep';
 
 const getLabels = async (): Promise<Label[]> => {
@@ -14,7 +14,25 @@ export const useLabels = () => {
         {
             queryKey: ['labels'],
             queryFn: getLabels,
-            staleTime: 1000 * 60 * 60 // Keep data fresh for an hour
+            staleTime: 1000 * 60 * 60, // Keep data fresh for an hour,
+            placeholderData: [
+                {
+                    id: 717031390,
+                    node_id: "MDU6TGFiZWw3MTcwMzEzOTA=",
+                    url: "https://api.github.com/repos/facebook/react/labels/good%20first%20issue",
+                    name: "good first issue",
+                    color: "6ce26a",
+                    default: "true"
+                },
+                {
+                    id: 717031392,
+                    node_id: "MDU6TGFiZWw3MTcwMzEzOTA=",
+                    url: "https://api.github.com/repos/facebook/react/labels/good%20first%20issue",
+                    name: "good first issue",
+                    color: "6ce26a",
+                    default: "true"
+                }
+            ]
         },
     );
 
